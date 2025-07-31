3c2d653ab3ea2b68d02817adab26ddf4709b86e0
Roshan Chourasia <croshanBTECH22@ced.alliance.edu.in>
2025-07-31 23:37:46 +0530

    made more responsive.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className="flex justify-between bg-[#1d3354] text-white py-3">
-            <div className="logo mx-9 font-bold md:text-2xl">
+            <div className="logo mx-9 font-bold text-xs sm:text-2xl">
                 <span>Tick-Task</span>
             </div>
             <ul className="flex gap-8 mx-9 items-center">
-                <li className="cursor-pointer hover:font-bold transistion-all">Home</li>
-                <li className="cursor-pointer hover:font-bold transistion-all">Your Tasks</li>
+                <li className="cursor-pointer hover:font-bold transistion-all text-xs sm:text-2xl">Home</li>
+                <li className="cursor-pointer hover:font-bold transistion-all text-xs sm:text-2xl">Your Tasks</li>
             </ul>
         </nav>
     )
